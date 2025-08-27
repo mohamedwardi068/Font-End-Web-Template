787639632248ae74fca591c9f6acb56f9118b763
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-08-27 21:50:51 +0100

    Done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+const App = () => {
+  return (
+/*     <Routes>
+      <Route index element={<></>} />
+      <Route path="/" element={<></>} />
+
+    </Routes> */
+    <div className="text-red-50"> a</div>
+  );
+};
+
+export default App;
